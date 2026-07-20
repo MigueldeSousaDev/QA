6eec2f680fdc33831610aa2aa5db20e3dc493b94
MigueldeSousaDev <miguelsousa355@gmail.com>
2026-07-20 16:15:39 +0100

    First commit and push

---
+++ src/test/java/VotaQuePassa/EleicaoTest.java
@@ -0,0 +1,45 @@
+package VotaQuePassa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EleicaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void adicionarCandidato() {
+    }
+
+    @Test
+    void votar() {
+    }
+
+    @Test
+    void contarVotos() {
+    }
+
+    @Test
+    void obterVencedor() {
+    }
+
+    @Test
+    void getNome() {
+    }
+
+    @Test
+    void getIdadeMinima() {
+    }
+
+    @Test
+    void getCandidatos() {
+    }
+
+    @Test
+    void getVotos() {
+    }
+}
